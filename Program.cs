6d62980a844977a97ffd8083677c432944f8064d
Alex <automex74@gmail.com>
2022-11-12 23:25:51 +0500

    add task_64

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+Console.WriteLine("--------------Задача №64.--------------");
+Console.WriteLine("Введите значение N ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine(PrintNumber(N, 1));    
+
+string PrintNumber(int start, int end)
+{
+    if(start == end) return start.ToString();
+    return (start + " " + PrintNumber(start - 1, end));
+}
+
+
+
