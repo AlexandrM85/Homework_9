8eee0e1fb079b911c4e8cb5ec203bb6d14ce1622
Alex <automex74@gmail.com>
2022-11-13 19:34:55 +0500

    add task_66

--- Program.cs
+++ Program.cs
@@ -10,5 +10,17 @@ string PrintNumber(int start, int end)
     return (start + " " + PrintNumber(start - 1, end));
 }
 
+// Задача 66: Задайте значения A и B. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+Console.WriteLine("--------------Задача №66.--------------");
+Console.WriteLine("Введите значение A ");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите значение B ");
+int B = int.Parse(Console.ReadLine()!);
+Console.WriteLine(SumElement(A, B));
 
+int SumElement(int x, int y)
+{
+    if (x == y) return y;
+    return x + SumElement(x + 1, y);
+}
 
